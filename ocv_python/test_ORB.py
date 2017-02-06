82fcdaeb4e5b57151ff25ae48caea468e04a1c40
Jason Klas <jason.j.klas@gmail.com>
2017-02-06 18:10:15 -0500

    Beginning testing image recognition by corner detection using ORB algorithm in python

---
+++ ocv_python/test_ORB.py
@@ -0,0 +1,63 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+""" Attempt to find specific car in parking lot - fails
+img1 = cv2.imread('lot1_car1.jpg',0)          # queryImage
+img2 = cv2.imread('lot1.jpg',0) # trainImage
+
+# Initiate SIFT detector
+orb = cv2.ORB_create()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = orb.detectAndCompute(img1,None)
+kp2, des2 = orb.detectAndCompute(img2,None)
+
+# create BFMatcher object
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+# Match descriptors.
+matches = bf.match(des1,des2)
+
+# Sort them in the order of their distance.
+matches = sorted(matches, key = lambda x:x.distance)
+
+# Draw first 10 matches.
+img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches[:10],None, flags=2)
+
+plt.imshow(img3),plt.show()
+"""
+
+""" identify keypoints in image - try it out!
+img = cv2.imread('lot1.jpg',0)
+
+# Initiate STAR detector
+orb = cv2.ORB_create()
+
+# find the keypoints with ORB
+kp = orb.detect(img,None)
+
+# compute the descriptors with ORB
+kp, des = orb.compute(img, kp)
+
+# draw only keypoints location,not size and orientation
+img2 = cv2.drawKeypoints(img,kp,None, color=(0,255,0), flags=0)
+plt.imshow(img2),plt.show()
+"""
+
+filename = 'lot1.jpg'
+img = cv2.imread(filename)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+gray = np.float32(gray)
+dst = cv2.cornerHarris(gray,2,3,0.04)
+
+#result is dilated for marking the corners, not important
+dst = cv2.dilate(dst,None)
+
+# Threshold for an optimal value, it may vary depending on the image.
+img[dst>0.01*dst.max()]=[0,0,255]
+
+cv2.imshow('dst',img)
+if cv2.waitKey(0) & 0xff == 27:
+    cv2.destroyAllWindows()
