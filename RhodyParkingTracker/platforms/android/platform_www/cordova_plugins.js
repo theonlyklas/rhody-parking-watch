9f5654ce29fb93580d47c5c9e789968b2a3b6b35
Jason Klas <jason.j.klas@gmail.com>
2017-04-03 19:04:49 -0400

    Added instructions on how to get Android emulator working

--- RhodyParkingTracker/platforms/android/platform_www/cordova_plugins.js
+++ RhodyParkingTracker/platforms/android/platform_www/cordova_plugins.js
@@ -12,9 +12,9 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.3.2",
+    "cordova-plugin-console": "1.0.6",
     "cordova-plugin-device": "1.1.5",
-    "cordova-plugin-console": "1.0.6"
+    "cordova-plugin-whitelist": "1.3.2"
 };
 // BOTTOM OF METADATA
 });
