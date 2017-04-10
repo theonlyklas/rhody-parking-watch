1e068f2126f5920009abdcbc4297ae397a5bf523
Jason Klas <jason.j.klas@gmail.com>
2017-04-10 15:26:54 -0400

    Remade project to fix connectivity error

--- RhodyParkingTracker/platforms/android/assets/www/cordova_plugins.js
+++ RhodyParkingTracker/platforms/android/assets/www/cordova_plugins.js
@@ -1,19 +1,8 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "id": "cordova-plugin-device.device",
-        "file": "plugins/cordova-plugin-device/www/device.js",
-        "pluginId": "cordova-plugin-device",
-        "clobbers": [
-            "device"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-console": "1.0.6",
-    "cordova-plugin-device": "1.1.5",
     "cordova-plugin-whitelist": "1.3.2"
 };
 // BOTTOM OF METADATA
