04c07c19f5195f02bdd747aad3a66904e55b3f57
Jason Klas <jason.j.klas@gmail.com>
2017-05-05 02:48:12 -0400

    Fully-working server communication, reflected in app

--- RhodyParkingTracker/www/js/index.js
+++ RhodyParkingTracker/www/js/index.js
@@ -10,7 +10,6 @@ window.onunload = function() {
   delete window.VIEWS;
   delete window.USER_CLASS;
   delete window.DESTINATION;
-  delete window.VIEWS;
   delete window.GPS_COORDINATES;
 
   /* WINDOW HTMLS */
@@ -36,10 +35,12 @@ function changeHTML(desiredView) {
     var htmlReplacement = window.AFTER_USER_TYPE_HTML;
   } else if (desiredView === "viewMyLots") {
     var htmlReplacement = window.VIEW_MY_LOTS_HTML;
+    detectParkingSpots("allUserLots");
   } else if (desiredView === "findClosest") {
     var htmlReplacement = window.FIND_CLOSEST_HTML;
   } else if (desiredView === "afterFindingLot") {
     var htmlReplacement = window.AFTER_FINDING_LOT_HTML;
+    detectParkingSpots(window.DESTINATION);
   } else if (desiredView === "userSelectionMenu") {
     var htmlReplacement = window.USER_SELECTION_MENU_HTML;
   }
@@ -58,57 +59,123 @@ function defineUser(desiredView, user) {
   changeHTML(desiredView);
 }
 
-/* tests server communication */
-function testPHP(button) {
+/* communicates with server to perform image recognition and update application view */
+function detectParkingSpots(requestedLot) {
   /* create xhttprequest object and initialize variables */
   var xhttp = new XMLHttpRequest();
   var url = "https:/jasonklas.me/rhodyparkingtracker/test.php";
 
-  if (button === "testPHP") {
-    var params = "test=true&name=jason";
+  /* if a requestedLot is passed to the function */
+  if (requestedLot === "BallentineHall") {
+    var params = "requestedLot=camera19";
+  } else if (requestedLot === "CBLSHall") {
+    var params = "requestedLot=camera24";
+  } else if (requestedLot === "Library") {
+    var params = "requestedLot=camera32";
+  } else if (requestedLot === "MemorialUnion") {
+    var params = "requestedLot=camera44";
+  } else if (requestedLot === "TylerHall") {
+    var params = "requestedLot=camera50";
+  } else if (requestedLot === "MackalGym") {
+    var params = "requestedLot=plainsrdA";
+  } else if (requestedLot === "FascitelliGym") {
+    var params = "requestedLot=plainsrdC";
+  } else if (requestedLot === "allUserLots") {
+    if (window.USER_CLASS === "visitor") {
+      detectParkingSpots("Library");
+      detectParkingSpots("CBLSHall");
+    } else if (window.USER_CLASS === "commuter") {
+      detectParkingSpots("MemorialUnion");
+      detectParkingSpots("MackalGym");
+    } else if (window.USER_CLASS === "resident") {
+      detectParkingSpots("MemorialUnion");
+      detectParkingSpots("FascitelliGym");
+    } else if (window.USER_CLASS === "faculty") {
+      detectParkingSpots("BallentineHall");
+      detectParkingSpots("TylerHall");
+    }
   }
 
-  xhttp.open("POST", url, true);
-  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  if (requestedLot != "allUserLots") {
+    window.DESTINATION = requestedLot;
+    window.GPS_COORDINATES = findParkingLot();
 
-  xhttp.onreadystatechange = function() {
-    if (xhttp.readyState === 4 && xhttp.status === 200) {
-      document.getElementById("appWrapper").innerHTML = xhttp.responseText;
-    }
-  };
+    xhttp.open("POST", url, true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-  xhttp.send(params);
+    xhttp.onreadystatechange = function() {
+      if (xhttp.readyState === 4 && xhttp.status === 200) {
+        console.log("success");
+
+        document.getElementById("statusMessage").innerHTML = "";
+        document.getElementById("lotsView").innerHTML += requestedLot + "<br>";
+
+        /* append the image of the lot */
+        var lotView = document.createElement("img");
+        lotView.setAttribute('src', "https://jasonklas.me/rhodyparkingtracker/" + params.slice(13) + ".png?" + Math.random());
+        document.getElementById("lotsView").appendChild(lotView);
+
+        /* append the status of the lot */
+        var lotStatus = document.createElement("p");
+        lotStatus.innerHTML = "Parking Lot: " + requestedLot;
+        if (xhttp.repsonseText == "False") {
+          lotStatus.innerHTML = "Sorry, there was an error retrieving the view.  Please try again."
+        } else {
+          lotStatus.innerHTML = xhttp.responseText;
+        }
+        document.getElementById("lotsView").appendChild(lotStatus);
+
+        /* append a button for GPS coordinates to the lot */
+        var GPSButton = document.createElement("a");
+        GPSButton.setAttribute("id", "GPSLink");
+        GPSButton.setAttribute("href", "http://maps.google.com/?q=" + window.GPS_COORDINATES)
+        GPSButton.innerHTML = '<button id="GPSButton" class="userButtons">Open in Google Maps</button>'
+        document.getElementById("lotsView").appendChild(GPSButton);
+        document.getElementById("lotsView").innerHTML += "<br>";
+      }
+    };
+
+    xhttp.send(params);
+  }
 }
 
 /* returns user to the screen they were previously viewing */
 function goBack() {
   var temp = window.VIEWS[(window.VIEWS.length - 2)];
-  window.VIEWS.pop(); 
+  window.VIEWS.pop();
   changeHTML(temp);
   window.VIEWS.pop();
 }
 
 /* saves the user's desired destination */
-function saveDestination(desiredDestination){
+function saveDestination(desiredDestination) {
   window.DESTINATION = desiredDestination;
   window.GPS_COORDINATES = findParkingLot();
 }
+
+/* saves the user's desired destination's coordinates */
 function findParkingLot() {
-  if (window.DESTINATION == 'Balentine'){
+  if (window.DESTINATION == 'BallentineHall') {
     //fine arts parking lot coordinates
-     return `41°29'17.5"N 71°31'24.3"W`;
-  } else if (window.DESTINATION == 'CBLSHall'){
-      //plains lot coordinates
-      return `41°29'25.4"N 71°32'15.7"W`;
-  } else if (window.DESTINATION == 'Library'){
+    return `41°29'17.5"N 71°31'24.3"W`;
+  } else if (window.DESTINATION == 'CBLSHall') {
+    //plains lot coordinates
+    return `41°29'25.4"N 71°32'15.7"W`;
+  } else if (window.DESTINATION == 'Library') {
     //dairy barn parking lot for residents..
-      return `41°29'21.0"N 71°32'01.9"W`;
-  } else if (window.DESTINATION == 'MemorialUnion'){
-      //keany lot coordinates
-      return `41°29'02.5"N 71°32'08.6"W`;
+    return `41°29'21.0"N 71°32'01.9"W`;
+  } else if (window.DESTINATION == 'MemorialUnion') {
+    //keany lot coordinates
+    return `41°29'02.5"N 71°32'08.6"W`;
   } else if (window.DESTINATION == 'TylerHall') {
     //plains lot coordinates
     return `41°29'17.5"N 71°31'24.3"W`;
+  } else if (window.DESTINATION == 'MackalGym') {
+    // Ryan Center parking lot coordinates
+    return `41°29'02.1"N 71°32'08.7"W`;
+  } else if (window.DESTINATION == 'FascitelliGym') {
+    // Fascitelli Gym parking lot coordinates
+    return `41°29'13.8"N 71°31'54.7"W`;
   }
 }
 
@@ -149,13 +216,11 @@ window.HELPFUL_LINKS_HTML = `
         <br>
     	<a href="https://github.com/theonlyklas/rhody-parking-watch"><button class="links">Github</button></a>
         <br>
-      <button class="links" onclick="testPHP('testPHP')">Test PHP</button>
-        <br>
     </div>
   </div>
 
   <div id="goBack">
-  <button class="goBack" onclick="goBack()">Go Back</button>
+    <button class="goBack" onclick="goBack()">Go Back</button>
   </div>
 `;
 
@@ -175,15 +240,15 @@ window.VIEW_MY_LOTS_HTML = `
   <div id="logo"></div>
   <div id="title">
     <p>My Available Lots</p>
+    <div id="lotsView">
+          <p id="statusMessage">Please wait, retrieving views of your lots</p>
+          <!-- filled by JavaScript -->
+    </div>
   </div>
-
-<img src="/js/Parking-Lot.jpg">
-<div id = "lot_info">
-<p> Lot Title     # Spots:  </p>
-<br>
-<div id="goBack">
-	<button class="goBack" onclick="goBack()">Go Back</button>
-</div>
+  <br>
+  <div id="goBack">
+  	<button class="goBack" onclick="goBack()">Go Back</button>
+  </div>
 
 `;
 
@@ -192,7 +257,8 @@ window.VIEW_MY_LOTS_HTML = `
 //Still working on passing a location into maps from the click
 window.FIND_CLOSEST_HTML = `
   <div id="logo"></div>
-  <br><br>
+  <br>
+  <br>
 <!-- dropdown menu for selecting distinations -->
 
   <div class="dropdown">
@@ -228,7 +294,7 @@ window.FIND_CLOSEST_HTML = `
         <th>Destinations</th>
       </tr>
       <tr>
-        <td> <button onclick="saveDestination('Balentine')">Balentine Hall</button></td>
+        <td> <button onclick="saveDestination('BallentineHall')">Ballentine Hall</button></td>
       </tr>
       <tr>
         <td> <button onclick="saveDestination('CBLSHall')">CBLS Hall</button></td>
@@ -240,7 +306,13 @@ window.FIND_CLOSEST_HTML = `
         <td> <button onclick="saveDestination('MemorialUnion')">Memorial Union</button></td>
       </tr>
       <tr>
-        <td> <tr onclick="saveDestination('TylerHall')"></td>
+        <td> <button onclick="saveDestination('TylerHall')">Tyler Hall</button></td>
+      </tr>
+      <tr>
+        <td> <button onclick="saveDestination('MackalGym')">Mackal-Tootell Gym</button></td>
+      </tr>
+      <tr>
+        <td> <button onclick="saveDestination('FascitelliGym')">Fascitelli Gym</button></td>
       </tr>
   </table>
 
@@ -250,7 +322,7 @@ window.FIND_CLOSEST_HTML = `
 
   <br>
   <div id="goBack">
-  <button class="goBack" onclick="goBack()">Go Back</button>
+    <button class="goBack" onclick="goBack()">Go Back</button>
   </div>
 
 `;
@@ -261,10 +333,13 @@ window.AFTER_FINDING_LOT_HTML = `
 
   <div id="title">
     <p>Closest Parking Lot</p>
+    <div id="lotsView">
+      <p id="statusMessage">Please wait, retrieving view of the lot</p>
+    </div>
     <br>
-    <a id="GPSLink" href="http://maps.google.com/?q=`+ window.GPS_COORDINATES +`"><button id="GPSButton" class="userButtons">Open in Google Maps</button></a>
+    <a id="GPSLink" href="http://maps.google.com/?q=` + window.GPS_COORDINATES + `"><button id="GPSButton" class="userButtons">Open in Google Maps</button></a>
     <br>
-    <div id="goBack">
+    <div id="goBack">a
     	<button class="goBack" onclick="goBack()">Go Back</button>
     </div>
   </div>
@@ -275,22 +350,22 @@ window.AFTER_FINDING_LOT_HTML = `
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
 function filterFunction() {
-    var input, filter, ul, li, a, i;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    div = document.getElementById("myDropdown");
-    a = div.getElementsByTagName("a");
-    for (i = 0; i < a.length; i++) {
-        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-            a[i].style.display = "";
-        } else {
-            a[i].style.display = "none";
-        }
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
     }
+  }
 }
 
 
--- ocv_python/Server Stuff/test.php
+++ ocv_python/Server Stuff/test.php
@@ -1,3 +1,9 @@
 <?php
-  echo $_POST
+  // set up the command to be run
+  $commandString = 'python3 /var/www/html/rhodyparkingtracker/car_detector.py ' . $_POST['requestedLot'] . ' 0';
+
+  // execute the command
+  $command = escapeshellcmd($commandString);
+  $output_including_status = exec($command);
+  echo $output_including_status;
 ?>
